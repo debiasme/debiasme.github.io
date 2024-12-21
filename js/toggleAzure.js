08277aa5382a3d4c4451ef418f1ddc83d8e7a82d
Manni Cheung <cmlmanni@gmail.com>
2024-12-21 01:21:53 +0000

    add azure logic

--- js/toggleAzure.js
+++ js/toggleAzure.js
@@ -4,6 +4,5 @@ export function toggleAzureAPI() {
   useAzure = !useAzure; // Toggle the useAzure flag
   const button = document.getElementById("toggle-azure");
   button.textContent = useAzure ? "Use Local API" : "Use Azure API";
-  console.log(useAzure);
   button.className = useAzure ? "toggle-azure-enabled" : "toggle-azure-disabled"; // Update class based on state
 } 
