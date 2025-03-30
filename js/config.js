db477589d9e79bc78e9573d26ccfd12d3dc24df4
Manni Cheung <cmlmanni@gmail.com>
2025-03-30 20:10:07 +0100

    update allowOrigins

--- js/config.js
+++ js/config.js
@@ -1,16 +1,24 @@
 const config = {
   development: {
-    apiUrl: 'http://localhost:3000',
-    allowedOrigins: ['http://localhost:3000', 'http://localhost:5501', 'http://127.0.0.1:5501']
+    apiUrl: "http://localhost:3000",
+    allowedOrigins: [
+      "http://localhost:3000",
+      "http://localhost:5501",
+      "http://127.0.0.1:5501",
+    ],
   },
   production: {
-    apiUrl: 'https://ayeeye.onrender.com',
-    allowedOrigins: ['https://cmlmanni.github.io/AyeEye']
-  }
+    apiUrl: "https://ayeeye.onrender.com",
+    allowedOrigins: [
+      "https://cmlmanni.github.io/AyeEye",
+      "https://debiasme.github.io",
+    ],
+  },
 };
 
 export const getConfig = () => {
-  const isDevelopment = window.location.hostname === 'localhost' || 
-                       window.location.hostname === '127.0.0.1';
-  return config[isDevelopment ? 'development' : 'production'];
+  const isDevelopment =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+  return config[isDevelopment ? "development" : "production"];
 };
